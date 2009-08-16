bf22af6544decb300e90175d05fb70091ee31883
Dan Davison <davison@stats.ox.ac.uk>
2009-08-15 22:48:28 -0400

    Temporary hack: create Biography instance for old Artist objects.

--- dbm.py
+++ dbm.py
@@ -495,6 +495,9 @@ class Root(Node):
         ## This needs to be replaced / is basically nonsense currently
         artists = (a for a in self.artists.values() if a.is_present())
         for artist in artists:
+            # TMP
+            if not hasattr(artist, 'biography'):
+                artist.biography = Biography(artist)
             artist.biography.update(dict(In_library=['Yes']))
             
     def write_similar_but_absent_biographies(self, direc, n=10):
